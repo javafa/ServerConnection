76b59155cf9a77b2b0ca1793d811580dcc04ccbe
javafa <nigne2016@gmail.com>
2017-06-29 17:12:53 +0900

    init

--- app/src/main/java/com/veryworks/android/serverconnection/MainActivity.java
+++ app/src/main/java/com/veryworks/android/serverconnection/MainActivity.java
@@ -2,14 +2,14 @@ package com.veryworks.android.serverconnection;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
-
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
-    public void goWrite(View view){
+    @Click
+    public void btnPost(){
         Intent intent = new Intent(this, WriteActivity.class);
         startActivity(intent);
     }
